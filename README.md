f67c84410c21ba266627380c7d70a17c5bc18fb5
mibra28 <michalbrakoniecki.it@gmail.com>
2024-01-31 16:30:10 +0100

    more test cases

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Project includes two test cases in CarsTests.java that checks creation of car object with POST method (https://retoolapi.dev/KCuEUu/cars):
-- createCarResponseIsSuccess
+Project includes tests cases in CarsTests.java that checks creation of car object with POST method (https://retoolapi.dev/KCuEUu/cars):
+- createCarResponseIsSuccessWithoutId
+- createCarResponseIsSuccessWithStringPrice
+- createCarResponseIsSuccessWithOnlyPrice
+- createCarResponseIsSuccessWithOnlyBrand
   * success code (201) and response body to include populated data
 - createCarWitDuplicatedIdFails
   * error code (500) and response body to include error message when attempt to create car with already existing id
+- 
--- src/test/java/CarCreationTests.java
+++ src/test/java/CarCreationTests.java
@@ -1,51 +1,96 @@
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.*;
 
 @JsonAutoDetect
 public class CarCreationTests {
 
     private static final String BASE_URL = "https://retoolapi.dev/KCuEUu/cars";
+    private static final String BRAND = "Brand";
+    private static final String PRICE = "price";
+    private static final String SUBARU = "Subaru";
+    private static final String ID = "id";
 
     @Test
-    public void createCarResponseIsSuccess() {
-        String expectedResponse = "\"Brand\": \"Subaru\",\n" + "  \"price\": 42";
+    public void createCarResponseIsSuccessWithoutId() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(BRAND, SUBARU);
+        map.put(PRICE, 42);
 
-        createCarScenario(201, "", expectedResponse);
+        createCarScenario(201, map)
+                .and()
+                .body(BRAND, equalTo(SUBARU),
+                        PRICE, equalTo(42),
+                        ID, notNullValue());
     }
 
     @Test
-    public void createCarWitDuplicatedIdFails() {
+    public void createCarResponseIsSuccessWithStringPrice() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(BRAND, SUBARU);
+        map.put(PRICE, "42");
+
+        createCarScenario(201, map)
+                .and()
+                .body(BRAND, equalTo(SUBARU),
+                        PRICE, equalTo("42"),
+                        ID, notNullValue());
+    }
+
+    @Test
+    public void createCarResponseIsSuccessWithOnlyPrice() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(PRICE, 42);
+
+        createCarScenario(201, map)
+                .and()
+                .body(BRAND, nullValue(),
+                        PRICE, equalTo(42),
+                        ID, notNullValue());
+    }
+
+    @Test
+    public void createCarResponseIsSuccessWithOnlyBrand() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(BRAND, SUBARU);
+
+        createCarScenario(201, map)
+                .and()
+                .body(BRAND, equalTo(SUBARU),
+                        PRICE, nullValue(),
+                        ID, notNullValue());
+    }
+
+    @Test
+    public void createCarWithDuplicatedIdFails() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(BRAND, SUBARU);
+        map.put(PRICE, 42);
+        map.put(ID, 1);
         String expectedResponse = "Insert failed, duplicate id";
 
-        createCarScenario(500, "1", expectedResponse);
+        createCarScenario(500, map)
+                .and()
+                .body(containsString(expectedResponse));
 
     }
 
-    private Map<String, Object> createCarParams(String id) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("Brand", "Subaru");
-        map.put("price", 42);
-        map.put("id", id);
-        return map;
-    }
-
-    private void createCarScenario(int statusCode, String carId, String responseToCheck) {
-        given()
+    private ValidatableResponse createCarScenario(int statusCode, Map<String, Object> params) {
+        return given()
                 .contentType(ContentType.JSON)
-                .body(createCarParams(carId))
+                .body(params)
                 .when()
                 .post(BASE_URL)
                 .then()
-                .assertThat().statusCode(statusCode)
-                .and()
-                .body(containsString(responseToCheck));
+                .assertThat()
+                .statusCode(statusCode);
     }
 
 }
